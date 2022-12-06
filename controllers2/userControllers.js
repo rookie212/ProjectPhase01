044ce5fbf7de4c582e24b439f3e919a39f11daea
rookie212 <ygtkilic@gmail.com>
2022-12-05 23:27:00 -0500

    bcrypt works

--- controllers2/userControllers.js
+++ controllers2/userControllers.js
@@ -4,20 +4,20 @@ const app = express();
 const clientSessions = require("client-sessions");
 
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
 
 
 const User = require('../models2/user');
 // const user = require("../models2/user");
 
 
-app.use(clientSessions({
-    cookieName: "session",
-    secret:"week12presentation",
-    duration: 3*60*1000, //3 minutes
-    activeDuration: 2*60*1000 //2 minutes for after request
+// app.use(clientSessions({
+//     cookieName: "session",
+//     secret:"week12presentation",
+//     duration: 3*60*1000, //3 minutes
+//     activeDuration: 2*60*1000 //2 minutes for after request
 
-}));
+// }));
 
 
 const welcomePage = async function (req,res){
@@ -34,28 +34,36 @@ const registerPage = async function (req,res){
 
 
 async function addNewUser(req, res) {
-    if (!req.body.email || !req.body.password || !req.body.age > 18 ||req.body.password !=req.body.password2) {
+    let uname = req.body.username;
+    let email = req.body.email;
+    let pwd = req.body.password;
+    let pwd2 = req.body.password2;
+    let ag = req.body.age;
+
+    if (!email || !pwd || !ag > 18 ||pwd !=pwd2) {
         return res.status(400).render("register",{ 'title': 'email and password is required, and be oler than 18 or passwords are not matching' });
     }
      
-    let a = req.body.email;
     try {
         console.log("adding user")
 
         
+
+        const alreadyMember = await User.findOne({email})
+            if (alreadyMember)
+                return res.render("login", {title: "This email is already registered"});
+              
+        cryptpwd = await bcrypt.hash(pwd, 3);
         const result = await User.create({
 
             
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password,
+            password: cryptpwd,
             age: req.body.age
         });
 
-        // const alreadyMember = await User.findOne({a}, (req,res)=>{
-        //     if (alreadyMember){
-        //         return res.render("login", {title: "This email is already registered"});
-        //     }  
+
         res.status(201).render("insert",
     
         // res.status(201).send(result);
@@ -77,29 +85,29 @@ async function addNewUser(req, res) {
 
 const tryingLogin = async function(req, res) {
 
-    const loginmail = req.body.email
+    const loginemail = req.body.email
     const loginpassword = req.body.password
     try{
-    if (!req.body.email || !req.body.password) {
+    if (!loginemail || !loginpassword) {
         return res.status(400).render("login",{'title': 'Email and password required' });
     }
 
-    const checkEmails = await User.findOne({email: req.body.email}).exec();
+    const checkEmails = await User.findOne({loginemail}).exec();
 
         if (!checkEmails) {
             return res.status(204).render("register",
             { "title": "You are not a member please register" });
         }
 
-        if(checkEmails){
-            const checkPasswords = await User.findOne({email: req.body.email, 
-                password: req.body.password}).exec();
+        if(checkEmails && await bcrypt.compare(loginpassword, cryptpwd)){
+            const checkPasswords = await User.findOne({loginemail, 
+                loginpassword}).exec();
 
                 if(!checkPasswords){
                     return res.status(204).render("login",
                     { "title": "Your password is wrong" });
                 }
-                const sessionUser = {email: req.body.email, password: req.body.password}
+                // const sessionUser = {email: req.body.email, password: req.body.password}
 
                 // req.session.sessionUser = {
                 //     email : loginmail,
@@ -119,13 +127,13 @@ const forgotMyPassword = async function(req, res) {
     { "title": "We forgot too create new one ;)" });
 }
 
-function ensureLogin(req, res, next){
-    if (!req.session.user){
-        res.render("login");
-    }else{
-        next();
-    }
-    };
+// function ensureLogin(req, res, next){
+//     if (!req.session.user){
+//         res.render("login");
+//     }else{
+//         next();
+//     }
+//     };
 
 
 module.exports = {
